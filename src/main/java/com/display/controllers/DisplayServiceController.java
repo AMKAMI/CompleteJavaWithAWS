11ad58efea40c0a148862a1b39fafc755d934809
AMKAMI <mr.amitkumarkutar@gmail.com>
2024-06-15 16:31:53 +0530

    adding logs

--- src/main/java/com/display/controllers/DisplayServiceController.java
+++ src/main/java/com/display/controllers/DisplayServiceController.java
@@ -1,13 +1,19 @@
 package com.display.controllers;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DisplayServiceController {
-
+	
+	private static final Logger logger = LogManager.getLogger(DisplayServiceController.class);
+	
 	@GetMapping("/get")
 	public String getResonse() {
+		logger.info("Inside get API of DisplayServiceController");
 		return "DisplayService Caller";
 	}
 	
